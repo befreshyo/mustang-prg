27dc33be1a7b3099b46cf4cd4e3ffbf5f0e925d3
lala <90055383+befreshyo@users.noreply.github.com>
2021-11-07 23:43:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 var contactURLArray = [];
 var contactArray = [];
 var loadingContact = 0;
+let statusCounter = 0;
 
 function initApplication() {
     console.log('mustang starting meow'); 
 }
 
-function loadIndex() {
-    var indexRequest = new XMLHttpRequest();
-    //indexRequest.open('GET', 'https://mustangindex-prg.azurewebsites.net/index.json'); // works perfect with my link
-    indexRequest.open('GET', 'https://mustang-index.azurewebsites.net/index.json');     // has console error
-    indexRequest.onload = function() {
-        console.log("index JSON:" + indexRequest.responseText);
-        document.getElementById("indexID").innerHTML = indexRequest.responseText;
-        contactIndex = JSON.parse(indexRequest.responseText);
-        contactURLArray.length = 0;
-        for (i=0; i<contactIndex.length; i++) {
-            contactURLArray.push(contactIndex[i].ContactURL);
-        }
-        console.log("contactURLArray: " + JSON.stringify(contactURLArray));
+async function loadIndex() {
+    const response = await fetch("https://mustang-index.azurewebsites.net/index.json")
+    const contactIndex = await response.text()
+
+    console.log("Index JSON:\n\n" + contactIndex);
+    document.getElementById("indexID").innerHTML = contactIndex
+    
+
+    const response2 = await fetch("https://mustang-index.azurewebsites.net/index.json")
+    const contactIndexJ = await response2.json()
+
+    for (i=0; i<contactIndexJ.length; i++) {
+        contactURLArray.push(contactIndexJ[i].ContactURL);
     }
-    indexRequest.send();
+    console.log("contactURL: " + JSON.stringify(contactURLArray));
 }
 
 function loadContacts() {
@@ -31,19 +32,21 @@ function loadContacts() {
     }
 }
 
-function loadNextContact(URL) {
+async function loadNextContact(URL) {
     console.log("url: " + URL);
-    contactRequest = new XMLHttpRequest();
-    //contactRequest.onreadystatechange = showContents;
-    contactRequest.open('GET', URL, true);
-    contactRequest.onreadystatechange = function() {
-        console.log(contactRequest.responseText);
-        var contact;
-        contact = JSON.parse(contactRequest.responseText);
-        console.log("contact: " + contact.firstName);
-        contactArray.push(contact);
-        document.getElementById("contactsID").innerHTML = JSON.stringify(contactArray);
-        if (contactRequest.readyState == 0) {
+    const response = await fetch(URL)
+    const contactResponse = await response.text()
+    contact = JSON.parse(contactResponse)
+    console.log(contactResponse)
+    console.log("Contact: " + contact.firstName);
+    contactArray.push(contact); 
+    document.getElementById("contactsID").innerHTML = JSON.stringify(contactArray);
+    console.log(contactArray)
+
+    //vince helped me with this the status. i originally had it printing if statements for contactRequest.readyState
+    //but it was not updating properly.
+
+/*   if (contactRequest.readyState == 0) {
             document.getElementById("statusID").innerHTML = "Uninitiated; object contains no data";
         } else if (contactRequest.readyState == 1) {
         document.getElementById("statusID").innerHTML = ":  Loading:  object is currently loading its data";
@@ -53,15 +56,25 @@ function loadNextContact(URL) {
             document.getElementById("statusID").innerHTML = "Interactive; user may interact with the object even though its is not fully loaded";
         } else if (contactRequest.readyState == 4) {
             document.getElementById("statusID").innerHTML = "Complete; object has finished initializing";}
+*/
 
-        loadingContact++;
-        if (contactURLArray.length > loadingContact) {
-            loadNextContact(contactURLArray[loadingContact]);
-        }
+    document.getElementById("statusID").innerHTML = "status: " + statusCounter;
+    i++;
+    statusCounter = "";
+    statusCounter = statusCounter + "loading contact#: " + i;
+    if(i => 38){
+        statusCounter = "";
+        statusCounter = statusCounter + "all contacts loaded";
     }
-    contactRequest.send(null);
-    //contactRequest.send();
+
+   
+
+    loadingContact++;
+    if (contactURLArray.length > loadingContact) {
+        loadNextContact(contactURLArray[loadingContact]);
+    }
 }
+
 
 function logContacts() {
     console.log(contactArray);
